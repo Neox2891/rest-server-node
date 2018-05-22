98a2a7ada3d5fe81407bb4a754c47ec1e68252ed
neox2891 <n.estrada2891@gmail.com>
2018-05-22 00:27:34 -0500

    Variables de entorno mondo

--- server/config/config.js
+++ server/config/config.js
@@ -16,9 +16,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb://localhost:27017/cafe'
+    urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:12345@ds229690.mlab.com:29690/cafe'
+    urlDB = MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
